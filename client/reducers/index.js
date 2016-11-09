698f24f75e05662423d46145b2ce8fa44fe217c8
jeffrey-meesters <jjeffreymeesters@gmail.com>
2016-11-09 17:40:01 +0100

    Created Loby and joined games

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,9 +1,11 @@
+import currentGame from './currentGame'
 import currentUser from './currentUser'
 import formErrors from './formErrors'
 import loading from './loading'
 import games from './games'
 
 module.exports = {
+  currentGame,
   currentUser,
   formErrors,
   games,
