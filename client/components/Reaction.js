3b9847a61da0b294ddfee133cda962fa393f914d
jeffrey-meesters <jjeffreymeesters@gmail.com>
2016-11-10 15:19:01 +0100

    Add OnClick and Onload for reaction and create times

--- client/components/Reaction.js
+++ client/components/Reaction.js
@@ -15,15 +15,43 @@ class Reaction extends Component {
     }, Math.random()*5000);
   }
 
+getReactionTime(event) {
+  let reactionTime = Date.now();
+  console.log(reactionTime);
+  return reactionTime;
+}
+
+getCreate(event) {
+  let create = Date.now();
+  console.log(create);
+  return create;
+
+}
+
+reactionTimePlayer() {
+  let timePlayer = getReactionTime() - getCreate();
+  console.log(timePlayer);
+  return timePlayer;
+}
+
   render() {
     let content = <div className="Ready"> 'READY?!' </div>;
 
     if (this.state.React) {
-      content = <img className="React-button" src = 'https://s3-us-west-2.amazonaws.com/chicagoview/icons/react-logo.png' />;
+      content = <img className="React-button" src = 'https://s3-us-west-2.amazonaws.com/chicagoview/icons/react-logo.png'
+                onClick={this.getReactionTime.bind(this)} onLoad={this.getCreate.bind(this)} />;
     }
     return (
-      <div >{content}</div>
-    );
+      <div>
+        <div className="Random">
+          {content}
+        </div>
+        <div className="time" >
+          Time: {this.reactionTimePlayer.bind(this)}
+        </div>
+      </div>
+      );
+
   }
 };
 
