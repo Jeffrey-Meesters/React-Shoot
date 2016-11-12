3ed7936b3673cb1e7ca0578fdf995b4849dbdc70
Robert Hopman <hopman.r@gmail.com>
2016-11-12 16:41:53 +0100

    final update with more ternary

--- client/components/Reaction.js
+++ client/components/Reaction.js
@@ -57,28 +57,31 @@ render() {
   const { game, currentUser } = this.props
 
   // at default (state of React = false) render this content
-  let content = <div className="Ready"> 'Get READY to click!' </div>;
+  let content = <div className="Ready"> Get ready to shoot! </div>;
 
 // at state of React = true render the image content with onLoad and onClick
   if (this.state.React) {
     content = <img className="React-button" src = 'https://s3-us-west-2.amazonaws.com/chicagoview/icons/react-logo.png'
-            onLoad={this.getCreateLogo.bind(this)}  onClick={this.getReaction.bind(this)} />;
+            onLoad={this.getCreateLogo.bind(this)}  onClick={this.getReaction.bind(this)}/>;
   }
 
   return (
-    <div>
-      <div className="Random">
+    <div className="reaction">
+    < img className="player-left" src = 'http://previews.123rf.com/images/antonbrand/antonbrand1402/antonbrand140200020/25989139-Cartoon-cowboy-drawing-guns-Isolated-on-white-Stock-Vector.jpg' />
+      <div className="Random"><br/>
         {/*this is where the the content gets rendered (? text : image)  */}
-        {content}
+        {content}<br/><br/>
       </div>
       <div className="time" >
         {/* render reaction time and name of player 1 and player 2*/}
         Reaction time {game.players[0] ? game.players[0].name : 'Waiting for player...'}: { game.p1Reaction } ms <br/>
-        Reaction time {game.players[1] ? game.players[1].name : 'Waiting for player...'}: { game.p2Reaction } ms
+         {game.players[1] ? `Reaction time ${game.players[1].name}: ${game.p2Reaction} ms` : 'Waiting for player...'}
       </div>
       <div className="winner">
-      winner is: {game.p2Reaction > game.p1Reaction ? game.players[1].name : game.players[0].name }
-      </div>
+      Winning time is: {game.p2Reaction > game.p1Reaction ? game.p1Reaction : game.p2Reaction}<br/>
+
+      </div>Great job cowboy!<br/>
+      < img className="player-right" src = 'http://previews.123rf.com/images/antonbrand/antonbrand1302/antonbrand130200018/18169122-Rugged-handsome-wild-west-cowboy-in-a-huge-hat-standing-poised-ready-to-draw-his-guns-in-a-gunfight--Stock-Vector.jpg' />
     </div>
     );
   };
